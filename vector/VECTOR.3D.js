765dce37bbe4ff63c8e1518dbfa1929a945bf2f5
iki1uc <entertrainer@web.de>
2026-07-27 02:34:22 +0200

    Add VECTOR_3D function to combine vector components

--- vector/VECTOR.3D.js
+++ vector/VECTOR.3D.js
@@ -1,1 +1,8 @@
+import { VECTOR_B } from "./VECTOR.B.js";
+import { VECTOR_H } from "./VECTOR.H.js";
+import { VECTOR_T } from "./VECTOR.T.js";
+
+export function VECTOR_3D(x,y,z){
+    return {...VECTOR_B(x), ...VECTOR_H(y), ...VECTOR_T(z)};
+}
 
